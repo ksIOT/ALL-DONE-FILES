b3f23c0b76bdccca98f845f9b717ec8f65b55c2b
ksIOT <78725895+ksIOT@users.noreply.github.com>
2021-02-13 16:43:14 +0530

    Update esp32_deepsleep_brokker.ino

--- esp32_deepsleep_brokker.ino
+++ esp32_deepsleep_brokker.ino
@@ -1,10 +1,32 @@
+/******************************************************************************
+
+Example Code for deepsleep   using MQTT
+by: deepak ,prateek
+
+  Settings: 
+  Board -> ESP32 DEv Module 
+  Baudrate -> 115200
+ 
+  MQTT Broker --> Mosquitto MQTT on Raspberry Pi 4 
+  
+//The code works on MQTT Protocol to send/receive data across the cloud.
+ 
+  
+//Edit ONLY the "Configuration" part of the code to make any changes to WiFi or MQTT settings.
+ 
+ 
+ This sketch connects the ESP32  to a MQTT broker and subcribes to the topic
+test/dht. 
+******************************************************************************/
+
 #include <WiFi.h>
 #include <PubSubClient.h>
+// want to change to the seconds change to the 1000000 for the seconds
+//want to change to the hour basis chage to the 3600000000 for the hour 
 
-#define uS_TO_S_FACTOR 3600000000  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  1
-/* Time ESP32 will go to sleep (in seconds) */
-
+#define uS_TO_S_FACTOR 3600000000  /* Conversion factor for micro seconds to hour */
+#define TIME_TO_SLEEP  1/* Time ESP32 will go to sleep (in hour) */
+ //*********************************** Configuration ************************************************* 
 RTC_DATA_ATTR int bootCount = 0;
  
 const char* ssid = "AudiR8";
@@ -17,6 +39,7 @@ const char* clientID = "ksiot";
 
 String temp_str;
 char temp[50];
+//*********************************************************************************************
 
 WiFiClient espClient;
 PubSubClient client(clientID,1883,espClient);
